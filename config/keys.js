0417d98110003342f41b950d5f46cb0b55fcc7e1
Allan <akanyijuka@gmail.com>
2018-11-07 18:25:40 +0100

    keys added

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//add the file to .gitignore
+
+module.exports={
+    google:{
+    clientID: '216414291099-vn09h4inhvh0v48ld53p8quphlraff6e.apps.googleusercontent.com',
+    clientSecret:'Rus1KPjVq94cpEnJfSaM-7MY'
+    }
+}
